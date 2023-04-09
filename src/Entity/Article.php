0d707744ef6a10ece88218d617f0276b45b460de
badiskefi <badiskefi@gmail.com>
2023-04-09 14:54:56 +0100

    TP6 Symfony6

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -32,6 +32,9 @@ class Article
     )]
     private ?string $prix = null;
 
+    /*#[ORM\ManyToOne(inversedBy: 'articles')]
+    private ?Category $category = null;*/
+
     public function getId(): ?int
     {
         return $this->id;
@@ -60,4 +63,16 @@ class Article
 
         return $this;
     }
+
+    /*public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
+
+        return $this;
+    }*/
 }
