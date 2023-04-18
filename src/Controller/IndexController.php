ec9791322ba074e5bfa3b662679ed13d77d7994f
badiskefi <badiskefi@gmail.com>
2023-04-18 18:36:17 +0100

    TP7 Symfony6

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,35 +2,100 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Article;
+use App\Entity\Category;
 use App\Form\ArticleType;
+use App\Form\CategoryType;
+use App\Form\PropertySearchType;
+use App\Entity\PropertySearch;
+use App\Entity\CategorySearch;
+use App\Entity\PriceSearch;
+use App\Form\CategorySearchType;
+use App\Form\PriceSearchType;
+
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
-use App\Entity\Category;
-use App\Form\CategoryType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class IndexController extends AbstractController
 {
     /**
-     *@Route("/",name="article_list")
-     */
-    public function home(ManagerRegistry $doctirine)
-    {
-        $entityManager = $doctirine->getManager();
-        //récupérer tous les articles de la table article de la BD
-        // et les mettre dans le tableau $articles
-        $articles = $doctirine->getRepository(Article::class)->findAll();
-        return $this->render('articles/index.html.twig', ['articles' => $articles]);
-    }
+ *@Route("/",name="article_list")
+ */
+ public function home(Request $request, ManagerRegistry $doctirine)
+ {
+    $entityManager = $doctirine->getManager();
 
+ $propertySearch = new PropertySearch();
+ $form = $this->createForm(PropertySearchType::class,$propertySearch);
+ $form->handleRequest($request);
+ //initialement le tableau des articles est vide,
+ //c.a.d on affiche les articles que lorsque l'utilisateur
+ //clique sur le bouton rechercher
+ $articles= [];
+
+ if($form->isSubmitted() && $form->isValid()) {
+ //on récupère le nom d'article tapé dans le formulaire
+ $nom = $propertySearch->getNom();
+ if ($nom!="")
+ //si on a fourni un nom d'article on affiche tous les articles ayant ce nom
+ $articles= $doctirine->getRepository(Article::class)->findBy(['nom' => $nom] );
+ else
+ //si si aucun nom n'est fourni on affiche tous les articles
+ $articles= $doctirine->getRepository(Article::class)->findAll();
+ }
+ return $this->render('articles/index.html.twig',[ 'form' =>$form->createView(), 'articles' => $articles]);
+ }
+/**
+ * @Route("/art_cat/", name="article_par_cat")
+ * Method({"GET", "POST"})
+ */
+public function articlesParCategorie(Request $request, ManagerRegistry $doctirine) {
+    $entityManager = $doctirine->getManager();
+
+    $categorySearch = new CategorySearch();
+    $form = $this->createForm(CategorySearchType::class,$categorySearch);
+    $form->handleRequest($request);
+    $articles= [];
+    if($form->isSubmitted() && $form->isValid()) {
+    $category = $categorySearch->getCategory();
+   
+    if ($category!="" )
+   $articles= $category->getArticles();
+    else
+    $articles= $doctirine->getRepository(Article::class)->findAll();
+    }
+   
+    return $this->render('articles/articlesParCategorie.html.twig',['form' => $form->createView(),'articles' => $articles]);
+    }
+    /**
+ * @Route("/art_prix/", name="article_par_prix")
+ * Method({"GET"})
+ */
+ public function articlesParPrix(Request $request, ManagerRegistry $doctirine)
+ {
+    $entityManager = $doctirine->getManager();
+
+
+ $priceSearch = new PriceSearch();
+ $form = $this->createForm(PriceSearchType::class,$priceSearch);
+ $form->handleRequest($request);
+ $articles= [];
+ if($form->isSubmitted() && $form->isValid()) {
+ $minPrice = $priceSearch->getMinPrice();
+ $maxPrice = $priceSearch->getMaxPrice();
+
+ $articles= $doctirine->getRepository(Article::class)->findByPriceRange($minPrice,$maxPrice);
+ }
+ return $this->render('articles/articlesParPrix.html.twig',[ 'form' =>$form->createView(), 'articles' => $articles]);
+ }
     /**
      * @Route("/article/save")
      */
